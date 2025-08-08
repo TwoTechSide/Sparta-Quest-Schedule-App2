880eef738f43e4656a9463256b6302cbf4458813
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-08 20:11:20 +0900

    feat: 로그인 확인 Filter 구현

--- src/main/java/com/scheduleapp2/exception/ErrorCode.java
+++ src/main/java/com/scheduleapp2/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum ErrorCode {
     SCHEDULE_NOT_FOUND("SCH-001", "일정을 찾을 수 없습니다."),
 
-    USER_NOT_FOUND("USR-001", "유저를 찾을 수 없습니다.");
+    USER_NOT_FOUND("USR-001", "유저를 찾을 수 없습니다."),
+    USER_NOT_LOGIN("USR-002", "로그인이 필요합니다.");
 
     private final String code;
     private final String message;
---
+++ src/main/java/com/scheduleapp2/filter/LoginFilter.java
@@ -0,0 +1,62 @@
+package com.scheduleapp2.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.scheduleapp2.dto.ErrorResponse;
+import com.scheduleapp2.exception.ErrorCode;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.PatternMatchUtils;
+
+import java.io.IOException;
+
+@Slf4j
+public class LoginFilter implements Filter {
+
+    private static final String[] WHITE_LIST = {
+            "/", "/users/signup", "/users/login",
+            "/v3/api-docs", "/v3/api-docs/*",
+            "/swagger-ui", "/swagger-ui/*"};
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String requestURI = request.getRequestURI();
+
+        // 1. 화이트 리스트(String[] WHITE_LIST)에 포함되지 않은 URI인 경우
+        // 2. session이 없거나, 유저가 로그인한 경우가 아니면(LOGIN_USER == null)
+        if (!isWhiteList(requestURI)) {
+            HttpSession session = request.getSession(false);
+            if (session == null || session.getAttribute("LOGIN_USER") == null) {
+
+                // jackson 라이브러리가 Java 8의 LocalDateTime을 지원하지 않기 때문에 모듈 추가
+                final ObjectMapper objectMapper = new ObjectMapper();
+                objectMapper.registerModule(new JavaTimeModule());
+
+                // 3. ErrorResponse ( USER_NOT_FOUND, UNAUTHORIZED ) 를 json 으로 변환한 뒤, response에 담아서 반환
+                final ErrorCode errorCode = ErrorCode.USER_NOT_FOUND;
+                final HttpStatus errorStatus = HttpStatus.UNAUTHORIZED;
+
+                ErrorResponse error = ErrorResponse.of(
+                        errorStatus,
+                        errorCode.getCode(),
+                        errorCode.getMessage(),
+                        request.getRequestURI());
+
+                response.setStatus(errorStatus.value());
+                response.setContentType("application/json;charset=UTF-8");
+                response.getWriter().write(objectMapper.writeValueAsString(error));
+            }
+        }
+    }
+
+    private boolean isWhiteList(String requestURI) {
+        return PatternMatchUtils.simpleMatch(WHITE_LIST, requestURI);
+    }
+}
