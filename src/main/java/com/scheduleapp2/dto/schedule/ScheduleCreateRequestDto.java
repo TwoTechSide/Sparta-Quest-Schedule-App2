963142386198932bcd4a29d36cddb8b0cb24cdf7
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-14 06:32:33 +0900

    hotfix: Size 어노테이션 예외 발생시 message 추가

--- src/main/java/com/scheduleapp2/dto/schedule/ScheduleCreateRequestDto.java
+++ src/main/java/com/scheduleapp2/dto/schedule/ScheduleCreateRequestDto.java
@@ -5,8 +5,10 @@ import jakarta.validation.constraints.Size;
 
 public record ScheduleCreateRequestDto(
         @NotBlank(message = "제목은 필수 입력 값입니다.")
-        @Size(min = 2, max = 40)
+        @Size(min = 2, max = 40, message = "제목은 2글자 이상 40글자 이하로 작성해야 합니다.")
         String title,
         @NotBlank(message = "내용은 필수 입력 값입니다.")
-        @Size(min = 2, max = 200)
-        String content) { }
+        @Size(min = 2, max = 200, message = "제목은 2글자 이상 200글자 이하로 작성해야 합니다.")
+        String content) {
+
+}
