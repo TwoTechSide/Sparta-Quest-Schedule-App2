9a2a12f496bfe74f2bd10aa7f78b173abf02519b
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-08-14 02:32:50 +0900

    refactor: CommentResponseDto -> id도 같이 반환, 코드 정리

--- src/main/java/com/scheduleapp2/dto/comment/CommentResponseDto.java
+++ src/main/java/com/scheduleapp2/dto/comment/CommentResponseDto.java
@@ -3,6 +3,7 @@ package com.scheduleapp2.dto.comment;
 import java.time.LocalDateTime;
 
 public record CommentResponseDto(
+        Long id,
         String content,
         LocalDateTime createdAt,
         LocalDateTime updatedAt) {
